e53ea56d864984e4709671b68e7c7f2229f5c047
vectormars <xuejie328@gmail.com>
2017-11-01 14:04:18 -0400

    Add files via upload

--- Data Structure/Vector_01.cpp
+++ Data Structure/Vector_01.cpp
@@ -8,7 +8,7 @@ using namespace std;
 main(){
 
     vector<int> q;
-    q.push_back(10); q.push_back(11); q.push_back(12);
+    q.push_back(10); q.push_back(20); q.push_back(30);
 
     vector<int> v;
     for(int i=0; i<5; ++i){
@@ -23,11 +23,14 @@ main(){
 
     /// Visit element
     printf("The first element in vector q is %d \n",*q.begin()); // 10
-    printf("The second element in vector q is %d \n",q[1]);      // 11
+    printf("The second element in vector q is %d \n",q[1]);      // 20
+    printf("The last element in vector q is %d \n",q.back());    // 30
     printf("The end element in vector q is %d \n",*q.end());     // 0
     printf("\n");
 
     /// Loop the vector
+    /// The function begin( ) returns an iterator to the start of the vector.
+    /// The function end( ) returns an iterator to the end of the vector.
     printf("vector v: \n");
     vector<int>::const_iterator i;
     for(i=v.begin(); i!=v.end(); ++i){
