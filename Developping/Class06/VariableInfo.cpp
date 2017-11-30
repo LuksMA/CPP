5e7bf1813ddd1f0760dd4aab4f1c85eebfe46ab7
vectormars <xuejie328@gmail.com>
2017-11-30 10:04:37 -0500

    Add files via upload

--- Developping/Class06/VariableInfo.cpp
+++ Developping/Class06/VariableInfo.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <map>
 #include <iostream>
-
+#include <unordered_map>
 
 using namespace std;
 
@@ -12,18 +12,23 @@ VariableInfo :: VariableInfo(int no, int type, vector<int> dataSetColumn){
         varNo = no;
         varType = type;
         map<vector<int>, vector<int>> mapDict;
+
         if(type==2){
             vector<int> uni = uniqueValues(dataSetColumn);
+            cuts = seq(10);
             mapDict = mapSubsets(uni,no);
         }else if(type==1){
             vector<int> uni = uniqueValues(dataSetColumn);
+            cuts = seq(uni.size());
             mapDict = mapSequence(uni,no);
         }else if(type==0){
             vector<int> uni = {0,1,2,3,4,5,6,7,8,9};
+            cuts = seq(10);
             mapDict = mapSequence(uni,no);
         }else{
             cout<<"\nIn correct input for data type.\n";
         }
+
         searchSets = mapDict;
     }
 
@@ -46,6 +51,14 @@ void VariableInfo :: getCombPrint(vector<int> vectIn){
     print1DVector(getComb(vectIn));
 }
 
+unordered_map<int,int> VariableInfo :: getCuts(){
+    return cuts;
+}
+
+void VariableInfo :: printCuts(){
+    printUnorderedMap(cuts);
+}
+
 void VariableInfo :: printVarInfo(){
     cout<<"Variable number: "<<getVarNo()<<"\tType: "<<getEnumType(dataType(getVarType()))<<endl;
     cout<<getCombSize()<<" available combinations:"<<endl;
@@ -55,6 +68,23 @@ void VariableInfo :: printVarInfo(){
         printOrdinalMap(searchSets);
     }
 }
+
+
+//void VariableInfo :: printCuts(){
+//    print1DVector(cuts);
+//}
+
+
+unordered_map<int,int> VariableInfo :: seq(int n){
+    unordered_map<int,int> mapOut;
+    for(unsigned i=1;i<=n;++i){
+        mapOut[i] = varNo;
+    }
+    return mapOut;
+}
+
+
+
 
 /************************************************************************************************************/
 vector<VariableInfo> creatVariableInfo(vector<vector<int>> dataSet,vector<int> varType){
--- Developping/Class06/VariableInfo.h
+++ Developping/Class06/VariableInfo.h
@@ -1,24 +1,29 @@
 #ifndef VARIABLEINFO_H
 #define VARIABLEINFO_H
-
+#include <unordered_map>
 
 class VariableInfo
 {
     private:
         int varNo;
         int varType;
+        unordered_map<int,int> cuts;
         map<vector<int>, vector<int>> searchSets;
+
     public:
         VariableInfo(int no, int type, vector<int> dataSetColumn);
         int getCombSize();
         int getVarNo();
         int getVarType();
+        unordered_map<int,int> getCuts();
         map<vector<int>, vector<int>> getCombs();
         vector<int> getComb(vector<int> vectIn);
         void getCombPrint(vector<int> vectIn);
         void printVarInfo();
+        void printCuts();
 
-
+    private:
+        unordered_map<int,int> seq(int n);
 };
 
 #endif // VARIABLEINFO_H
