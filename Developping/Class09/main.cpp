7f5421a11d4d28298a5fca57b1d361363d8a3e3f
vectormars <xuejie328@gmail.com>
2017-12-08 10:32:43 -0500

    Add files via upload

--- Developping/Class09/main.cpp
+++ Developping/Class09/main.cpp
@@ -30,11 +30,20 @@ void threeDepthPrint(DataGeneration &data, vector<Patient *> patients)
                 int bestIndexLocal = 0;
                 int indexi = 0, indexj = 0, indexk = 0;
                 int cuti = 0, cutj = 0, cutk = 0;
-                for(auto xi : { 1,2,3,4,5,6,7,8,9,10 })            // loop4
+                for(auto xi :
+                        {
+                            1,2,3,4,5,6,7,8,9,10
+                        })            // loop4
                 {
-                    for(auto xj : { 1,2,3,4,5,6,7,8,9,10 })    // loop5
+                    for(auto xj :
+                            {
+                                1,2,3,4,5,6,7,8,9,10
+                            })    // loop5
                     {
-                        for(auto xk : { 1,2,3,4,5,6,7,8,9,10 })    // loop6
+                        for(auto xk :
+                                {
+                                    1,2,3,4,5,6,7,8,9,10
+                                })    // loop6
                         {
                             double v0000 = 0.0, v0001 = 0.0, v0010 = 0.0, v0011 = 0.0;
                             double v0100 = 0.0, v0101 = 0.0, v0110 = 0.0, v0111 = 0.0;
@@ -53,22 +62,22 @@ void threeDepthPrint(DataGeneration &data, vector<Patient *> patients)
                                         {
                                             if(flag[3])
                                             {
-                                                v0000 += p->getY(0);
+                                                v0000 += *p->getYPtr();
                                             }
                                             else
                                             {
-                                                v0001 += p->getY(0);
+                                                v0001 += *p->getYPtr();
                                             }
                                         }
                                         else
                                         {
                                             if(flag[3])
                                             {
-                                                v0010 += p->getY(0);
+                                                v0010 += *p->getYPtr();
                                             }
                                             else
                                             {
-                                                v0011 += p->getY(0);
+                                                v0011 += *p->getYPtr();
                                             }
                                         }
                                     }
@@ -78,22 +87,22 @@ void threeDepthPrint(DataGeneration &data, vector<Patient *> patients)
                                         {
                                             if(flag[3])
                                             {
-                                                v0100 += p->getY(0);
+                                                v0100 += *p->getYPtr();
                                             }
                                             else
                                             {
-                                                v0101 += p->getY(0);
+                                                v0101 += *p->getYPtr();
                                             }
                                         }
                                         else
                                         {
                                             if(flag[3])
                                             {
-                                                v0110 += p->getY(0);
+                                                v0110 += *p->getYPtr();
                                             }
                                             else
                                             {
-                                                v0111 += p->getY(0);
+                                                v0111 += *p->getYPtr();
                                             }
                                         }
                                     }
@@ -106,22 +115,22 @@ void threeDepthPrint(DataGeneration &data, vector<Patient *> patients)
                                         {
                                             if(flag[3])
                                             {
-                                                v1000 += p->getY(0);
+                                                v1000 += *p->getYPtr();
                                             }
                                             else
                                             {
-                                                v1001 += p->getY(0);
+                                                v1001 += *p->getYPtr();
                                             }
                                         }
                                         else
                                         {
                                             if(flag[3])
                                             {
-                                                v1010 += p->getY(0);
+                                                v1010 += *p->getYPtr();
                                             }
                                             else
                                             {
-                                                v1011 += p->getY(0);
+                                                v1011 += *p->getYPtr();
                                             }
                                         }
                                     }
@@ -131,22 +140,22 @@ void threeDepthPrint(DataGeneration &data, vector<Patient *> patients)
                                         {
                                             if(flag[3])
                                             {
-                                                v1100 += p->getY(0);
+                                                v1100 += *p->getYPtr();
                                             }
                                             else
                                             {
-                                                v1101 += p->getY(0);
+                                                v1101 += *p->getYPtr();
                                             }
                                         }
                                         else
                                         {
                                             if(flag[3])
                                             {
-                                                v1110 += p->getY(0);
+                                                v1110 += *p->getYPtr();
                                             }
                                             else
                                             {
-                                                v1111 += p->getY(0);
+                                                v1111 += *p->getYPtr();
                                             }
                                         }
                                     }
@@ -192,7 +201,165 @@ void threeDepthPrint(DataGeneration &data, vector<Patient *> patients)
     }
 }
 
+class solution
+{
+    double* result;
+    int* cuts;
+public:
+    solution(double r[8], int c[6])
+    {
+        result = r;
+        cuts = c;
+    }
+    ~solution() {}
+};
 
+
+
+
+vector<solution *> threeDepth(DataGeneration &data, vector<Patient *> patients)
+{
+//    double T0 = data.getSumT0();
+    int sampleSize = data.getCovariateSize();
+    vector<solution *> solutions;
+    solutions.reserve(20000000);
+    for(int i=0; i<sampleSize; ++i)   // loop1
+    {
+        for(int j=i+1; j<sampleSize; ++j)    // loop2
+        {
+            for(int k=j+1; k<sampleSize; ++k)    // loop3
+            {
+                for(auto xi : { 1,2,3,4,5,6,7,8,9,10 })            // loop4
+                {
+                    for(auto xj : { 1,2,3,4,5,6,7,8,9,10 })    // loop5
+                    {
+                        for(auto xk : { 1,2,3,4,5,6,7,8,9,10 })    // loop6
+                        {
+                            double v0000 = 0.0, v0001 = 0.0, v0010 = 0.0, v0011 = 0.0;
+                            double v0100 = 0.0, v0101 = 0.0, v0110 = 0.0, v0111 = 0.0;
+                            double v1000 = 0.0, v1001 = 0.0, v1010 = 0.0, v1011 = 0.0;
+                            double v1100 = 0.0, v1101 = 0.0, v1110 = 0.0, v1111 = 0.0;
+
+                            for(auto p : patients)
+                            {
+                                bitset<4> flag = p->criteria(i,xi,j,xj,k,xk);
+
+                                if(flag[0])
+                                {
+                                    if(flag[1])
+                                    {
+                                        if(flag[2])
+                                        {
+                                            if(flag[3])
+                                            {
+                                                v0000 += *p->getYPtr();
+                                            }
+                                            else
+                                            {
+                                                v0001 += *p->getYPtr();
+                                            }
+                                        }
+                                        else
+                                        {
+                                            if(flag[3])
+                                            {
+                                                v0010 += *p->getYPtr();
+                                            }
+                                            else
+                                            {
+                                                v0011 += *p->getYPtr();
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if(flag[2])
+                                        {
+                                            if(flag[3])
+                                            {
+                                                v0100 += *p->getYPtr();
+                                            }
+                                            else
+                                            {
+                                                v0101 += *p->getYPtr();
+                                            }
+                                        }
+                                        else
+                                        {
+                                            if(flag[3])
+                                            {
+                                                v0110 += *p->getYPtr();
+                                            }
+                                            else
+                                            {
+                                                v0111 += *p->getYPtr();
+                                            }
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    if(flag[1])
+                                    {
+                                        if(flag[2])
+                                        {
+                                            if(flag[3])
+                                            {
+                                                v1000 += *p->getYPtr();
+                                            }
+                                            else
+                                            {
+                                                v1001 += *p->getYPtr();
+                                            }
+                                        }
+                                        else
+                                        {
+                                            if(flag[3])
+                                            {
+                                                v1010 += *p->getYPtr();
+                                            }
+                                            else
+                                            {
+                                                v1011 += *p->getYPtr();
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if(flag[2])
+                                        {
+                                            if(flag[3])
+                                            {
+                                                v1100 += *p->getYPtr();
+                                            }
+                                            else
+                                            {
+                                                v1101 += *p->getYPtr();
+                                            }
+                                        }
+                                        else
+                                        {
+                                            if(flag[3])
+                                            {
+                                                v1110 += *p->getYPtr();
+                                            }
+                                            else
+                                            {
+                                                v1111 += *p->getYPtr();
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            solutions.push_back(new solution(new double[8]{v0000 - v0001,v0010 - v0011,v0100 - v0101,v0110 - v0111,v1000 - v1001, v1010 - v1011,v1100 - v1101,v1110 - v1111},new int[6]{i,j,k,xi,xj,xk} ));
+                        } // end loop 6
+                    } // end loop 5
+                } // end loop 4
+            } // end loop 3
+        } // end loop 2
+    } // end loop 1
+    return solutions;
+}
 
 
 
@@ -207,7 +374,7 @@ int main()
     vector<int> rangesAction = {0,1};
 
     DataGeneration data(varType,rangesY,rangesAction,rangesCont,rangesOrd,rangesNom);
-    data.creatSamples(30);
+    data.creatSamples(3000);
     data.preprocessing();
 //    data.printInfo(6);
 
@@ -217,7 +384,10 @@ int main()
 //    pData.printPatients(patients);
 
     /// Three Depth
-    threeDepthPrint(data,patients);
+//    threeDepthPrint(data,patients);
+//    threeDepth(data,patients);
+
+
 
 
 
@@ -231,82 +401,27 @@ int main()
 
 
 /*****************************************************************************************************************************************************/
-template<class T>
-void print1DVector(vector<T> const &vectIn)
+bitset<3> binaryBit(int &num)
 {
-    cout<<"[ ";
-    for(const auto &row : vectIn)
+    switch(num)
     {
-        cout<<row<<' ';
-    }
-    cout<<']';
-}
-
-//vector<int> binary(int num){
-//    switch(num){
-//        case 0: return {-1,-1,-1};
-//        case 1: return {-1,-1,1};
-//        case 2: return {-1,1,-1};
-//        case 3: return {-1,1,1};
-//        case 4: return {1,-1,-1};
-//        case 5: return {1,-1,1};
-//        case 6: return {1,1,-1};
-//        case 7: return {1,1,1};
-//    }
-//    return {0,0,0};
-//}
-
-bitset<3> binaryBit(int &num){
-    switch(num){
-        case 0: return bitset<3> {0};
-        case 1: return bitset<3> {1};
-        case 2: return bitset<3> {2};
-        case 3: return bitset<3> {3};
-        case 4: return bitset<3> {4};
-        case 5: return bitset<3> {5};
-        case 6: return bitset<3> {6};
-        case 7: return bitset<3> {7};
+    case 0:
+        return bitset<3> {0};
+    case 1:
+        return bitset<3> {1};
+    case 2:
+        return bitset<3> {2};
+    case 3:
+        return bitset<3> {3};
+    case 4:
+        return bitset<3> {4};
+    case 5:
+        return bitset<3> {5};
+    case 6:
+        return bitset<3> {6};
+    case 7:
+        return bitset<3> {7};
     }
     return 0;
 }
 
-//vector<int> binary(int num)
-//{
-//    if(num == 0)
-//    {
-//        return {-1,-1,-1};
-//    }
-//    else if(num == 1)
-//    {
-//        return {-1,-1,1};
-//    }
-//    else if(num == 2)
-//    {
-//        return {-1,1,-1};
-//    }
-//    else if(num == 3)
-//    {
-//        return {-1,1,1};
-//    }
-//    else if(num == 4)
-//    {
-//        return {1,-1,-1};
-//    }
-//    else if(num == 5)
-//    {
-//        return {1,-1,1};
-//    }
-//    else if(num == 6)
-//    {
-//        return {1,1,-1};
-//    }
-//    else if(num == 7)
-//    {
-//        return {1,1,1};
-//    }
-//    else
-//    {
-//        cout<<"Incorrect input in binary function.\n";
-//        return {0};
-//    }
-//}
