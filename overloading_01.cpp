41f7aa0f3d49326d5143f41cc16b88f346f653e2
vectormars <xuejie328@gmail.com>
2017-12-14 14:49:09 -0500

    Add files via upload

---
+++ overloading_01.cpp
@@ -0,0 +1,35 @@
+// increment counter variable with ++ operator
+#include <iostream>
+using namespace std;
+////////////////////////////////////////////////////////////////
+class Counter
+{
+private:
+    unsigned int count; //count
+public:
+    Counter() : count(0) //constructor
+    { }
+    unsigned int get_count() //return count
+    {
+        return count;
+    }
+    void operator ++ () //increment (prefix)
+    {
+        ++count;
+    }
+};
+////////////////////////////////////////////////////////////////
+int main()
+{
+    Counter c1, c2; //define and initialize
+    cout << "c1=" << c1.get_count()<<endl; //display
+    cout << "c2=" << c2.get_count()<<endl;
+    ++c1; //increment c1
+    ++c2; //increment c2
+    ++c2; //increment c2
+
+    cout << "After ++:\n";
+    cout << "c1=" << c1.get_count()<<endl; //display again
+    cout << "c2=" << c2.get_count()<<endl;
+    return 0;
+}
