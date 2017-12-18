768b33d98ec4135e3c9dd1d34fc7393a425115e5
vectormars <xuejie328@gmail.com>
2017-12-18 14:06:28 -0500

    Add files via upload

---
+++ Developping/Class11/Patient.h
@@ -0,0 +1,71 @@
+#ifndef PATIENT_H
+#define PATIENT_H
+
+//#include "VariableInfo.h"
+
+#include <vector>
+#include <map>
+#include <stdio.h>
+#include <iostream>
+#include <random>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <bitset>
+
+using namespace std;
+
+class Patient
+{
+    private:
+        int patientID;
+        vector<int> action;
+        vector<int> valueX;
+        vector<double> valueY;
+        vector<int> type;
+        int actionBool;
+        int *xArray;
+        double *yArray;
+
+    public:
+        Patient(int ID, vector<int> const &a, vector<int> &X, vector<double> &Y, vector<int> const &t);
+        ~Patient();
+
+
+        int getID();
+        vector<int> getAction();
+        int getAction(int i);
+        vector<int> getX();
+        int getX(int i);
+        vector<double> getY();
+        double getY(int i);
+        double *getYPtr() { return yArray;}
+
+        vector<int> getDataType();
+        int getDataType(int i);
+        int getSizeAction();
+        int getSizeX();
+        int getSizeY();
+        void summary();
+
+        bool criteriaAction();
+        int criteriaActionInt();
+        int criteria(int var1, int cut1, int var2, int cut2, int var3, int cut3);
+
+        void printPatientID();
+        void printPatientX();
+        void printPatientAction();
+        void printPatientY();
+        void printPatients();
+        void printPatientsX();
+        void printPatientsXA();
+
+    private:
+        template<class T> void print1DVector(vector<T> const &vectIn);
+        bool criteria(int varNo, int cutNo);
+        int criteriaInt(int varNo, int cutNo);
+
+};
+
+#endif // PATIENT_H
---
+++ Developping/Class11/main.cpp
@@ -0,0 +1,293 @@
+#include <iostream>
+#include <omp.h>
+
+#include "DataGeneration.h"
+#include "Patient.h"
+#include "PatientData.h"
+
+
+using namespace std;
+
+
+
+
+
+
+//void threeDepthPrint(DataGeneration &data, vector<Patient *> patients)
+//{
+//    double T0 = data.getSumT0();
+//    int sampleSize = data.getCovariateSize();
+////int sampleSize = 5;
+//    for(int i=0; i<sampleSize; ++i)   // loop1
+//    {
+//        for(int j=i+1; j<sampleSize; ++j)    // loop2
+//        {
+//            for(int k=j+1; k<sampleSize; ++k)    // loop3
+//            {
+//                double bestLocal = 0.0;
+//                int bestIndexLocal = 0;
+//                int indexi = 0, indexj = 0, indexk = 0;
+//                int cuti = 0, cutj = 0, cutk = 0;
+//                for(auto xi : {1,2,3,4,5,6,7,8,9,10})            // loop4
+//                {
+//                    for(auto xj :{1,2,3,4,5,6,7,8,9,10})    // loop5
+//                    {
+//                        for(auto xk :{1,2,3,4,5,6,7,8,9,10})    // loop6
+//                        {
+//                            double v0000 = 0.0, v0001 = 0.0, v0010 = 0.0, v0011 = 0.0;
+//                            double v0100 = 0.0, v0101 = 0.0, v0110 = 0.0, v0111 = 0.0;
+//                            double v1000 = 0.0, v1001 = 0.0, v1010 = 0.0, v1011 = 0.0;
+//                            double v1100 = 0.0, v1101 = 0.0, v1110 = 0.0, v1111 = 0.0;
+//
+//                            for(auto p : patients)
+//                            {
+//                                bitset<4> flag = p->criteria(i,xi,j,xj,k,xk);
+//
+//                                if(flag[0])
+//                                {
+//                                    if(flag[1])
+//                                    {
+//                                        if(flag[2])
+//                                        {
+//                                            if(flag[3])
+//                                            {
+//                                                v0000 += *p->getYPtr();
+//                                            }
+//                                            else
+//                                            {
+//                                                v0001 += *p->getYPtr();
+//                                            }
+//                                        }
+//                                        else
+//                                        {
+//                                            if(flag[3])
+//                                            {
+//                                                v0010 += *p->getYPtr();
+//                                            }
+//                                            else
+//                                            {
+//                                                v0011 += *p->getYPtr();
+//                                            }
+//                                        }
+//                                    }
+//                                    else
+//                                    {
+//                                        if(flag[2])
+//                                        {
+//                                            if(flag[3])
+//                                            {
+//                                                v0100 += *p->getYPtr();
+//                                            }
+//                                            else
+//                                            {
+//                                                v0101 += *p->getYPtr();
+//                                            }
+//                                        }
+//                                        else
+//                                        {
+//                                            if(flag[3])
+//                                            {
+//                                                v0110 += *p->getYPtr();
+//                                            }
+//                                            else
+//                                            {
+//                                                v0111 += *p->getYPtr();
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                                else
+//                                {
+//                                    if(flag[1])
+//                                    {
+//                                        if(flag[2])
+//                                        {
+//                                            if(flag[3])
+//                                            {
+//                                                v1000 += *p->getYPtr();
+//                                            }
+//                                            else
+//                                            {
+//                                                v1001 += *p->getYPtr();
+//                                            }
+//                                        }
+//                                        else
+//                                        {
+//                                            if(flag[3])
+//                                            {
+//                                                v1010 += *p->getYPtr();
+//                                            }
+//                                            else
+//                                            {
+//                                                v1011 += *p->getYPtr();
+//                                            }
+//                                        }
+//                                    }
+//                                    else
+//                                    {
+//                                        if(flag[2])
+//                                        {
+//                                            if(flag[3])
+//                                            {
+//                                                v1100 += *p->getYPtr();
+//                                            }
+//                                            else
+//                                            {
+//                                                v1101 += *p->getYPtr();
+//                                            }
+//                                        }
+//                                        else
+//                                        {
+//                                            if(flag[3])
+//                                            {
+//                                                v1110 += *p->getYPtr();
+//                                            }
+//                                            else
+//                                            {
+//                                                v1111 += *p->getYPtr();
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                            }
+//
+//                            double sum1 = v0000 - v0001;
+//                            double sum2 = v0010 - v0011;
+//                            double sum3 = v0100 - v0101;
+//                            double sum4 = v0110 - v0111;
+//                            double sum5 = v1000 - v1001;
+//                            double sum6 = v1010 - v1011;
+//                            double sum7 = v1100 - v1101;
+//                            double sum8 = v1110 - v1111;
+//
+//                            double sum[] = {sum1,sum2,sum3,sum4,sum5,sum6,sum7,sum8};
+//
+//                            const auto ptr = max_element(sum,sum+8);
+//                            double temp = *ptr + T0;
+//
+//                            if(temp > bestLocal)
+//                            {
+//                                bestLocal = temp;
+//                                bestIndexLocal = distance(sum, ptr);
+//                                indexi = i;
+//                                indexj = j;
+//                                indexk = k;
+//                                cuti = xi;
+//                                cutj = xj;
+//                                cutk = xk;
+//                            }
+//                        }
+//                    }
+//                }
+//                bitset<3> bIndex = binaryBit(bestIndexLocal);
+//                cout<<bestLocal*2/data.getSampleSize()<<" Index: "
+//                    <<"["<<indexi<<" "<<cuti<<" "<<bIndex[2]
+//                    <<"] ["<<indexj<<" "<<cutj<<" "<<bIndex[1]
+//                    <<"] ["<<indexk<<" "<<cutk<<" "<<bIndex[0]
+//                    <<"]"<<endl;
+//            }
+//        }
+//    }
+//}
+
+
+class Result{
+private:
+    float value;
+    unsigned short info[3];
+public:
+    Result(double v, unsigned short varI, unsigned short cutI, unsigned short dirI, unsigned short varJ, unsigned short cutJ, unsigned short dirJ, unsigned short varK, unsigned short cutK, unsigned short dirK){
+        value = (float)v;
+        info[0] = cutI + varI*100 + dirI*10000;
+        info[1] = cutJ + varJ*100 + dirJ*10000;
+        info[2] = cutK + varK*100 + dirK*10000;
+    };
+    ~Result(){};
+};
+
+
+
+
+vector<Result *> threeDepth(DataGeneration &data, vector<Patient *> patients)
+{
+    int sampleSize = data.getCovariateSize();
+    vector<Result *> solutions;
+    solutions.reserve(20000000);
+
+    double v16[16];
+
+    for(unsigned short i=0; i<sampleSize; ++i)   // loop1
+    {
+        for(unsigned short j=i+1; j<sampleSize; ++j)    // loop2
+        {
+            for(unsigned short k=j+1; k<sampleSize; ++k)    // loop3
+            {
+                for(auto xi : { 1,2,3,4,5,6,7,8,9,10 })            // loop4
+                {
+                    for(auto xj : { 1,2,3,4,5,6,7,8,9,10 })    // loop5
+                    {
+                        for(auto xk : { 1,2,3,4,5,6,7,8,9,10 })    // loop6
+                        {
+
+                            std::fill_n(v16,16,0.0);
+                            for(auto p : patients)
+                            {
+                                int x = p->criteria(i,xi,j,xj,k,xk);
+                                v16[x] += *p->getYPtr();
+                            }
+                            solutions.push_back(new Result(v16[0]-v16[1],i,xi,0,j,xj,0,k,xk,0));
+                            solutions.push_back(new Result(v16[2]-v16[3],i,xi,0,j,xj,0,k,xk,1));
+                            solutions.push_back(new Result(v16[4]-v16[5],i,xi,0,j,xj,1,k,xk,0));
+                            solutions.push_back(new Result(v16[6]-v16[7],i,xi,0,j,xj,1,k,xk,1));
+                            solutions.push_back(new Result(v16[8]-v16[9],i,xi,1,j,xj,0,k,xk,0));
+                            solutions.push_back(new Result(v16[10]-v16[11],i,xi,1,j,xj,0,k,xk,1));
+                            solutions.push_back(new Result(v16[12]-v16[13],i,xi,1,j,xj,1,k,xk,0));
+                            solutions.push_back(new Result(v16[14]-v16[15],i,xi,1,j,xj,1,k,xk,1));
+                        } // end loop 6
+                    } // end loop 5
+                } // end loop 4
+            } // end loop 3
+        } // end loop 2
+    } // end loop 1
+    return solutions;
+}
+
+
+
+
+int main()
+{
+    /// Test data generation class
+    vector<int> varType(35,0); /// 0 for cont, 1 for ordinal, 2 for nominal
+    vector<double> rangesCont = {10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0};
+    vector<int> rangesOrd = {};
+    vector<int> rangesNom = {};
+    vector<double> rangesY = {0.0,100.0};
+    vector<int> rangesAction = {0,1};
+
+    DataGeneration data(varType,rangesY,rangesAction,rangesCont,rangesOrd,rangesNom);
+    data.creatSamples(3000);
+    data.preprocessing();
+//    data.printInfo(6);
+
+    /// Create patient objects
+    PatientData pData(data);
+    vector<Patient *> patients = pData.getPatients();
+//    pData.printPatients();
+
+    /// Three Depth
+//    threeDepthPrint(data,patients);
+    threeDepth(data,patients);
+
+
+
+
+
+
+
+
+
+
+}
+
+
