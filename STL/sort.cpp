a86e61bfd72987c7cfce605acccc13caca2d4cce
vectormars <xuejie328@gmail.com>
2017-12-19 13:10:46 -0500

    Add files via upload

---
+++ STL/sort.cpp
@@ -0,0 +1,16 @@
+// sorts an array of integers
+#include <iostream>
+#include <algorithm>
+using namespace std;
+//array of numbers
+int arr[] = {45, 2, 22, -17, 0, -30, 25, 55};
+int main()
+{
+    sort(arr, arr+8); //sort the numbers
+    for(int j=0; j<8; j++) //display sorted array
+        cout << arr[j] << ' ';
+    cout << endl;
+    return 0;
+}
+
+//-30 -17 0 2 22 25 45 55
