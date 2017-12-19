7e691994a14922e9925c88a0ee434a191fe5cda3
vectormars <xuejie328@gmail.com>
2017-12-19 13:48:27 -0500

    Add files via upload

---
+++ STL/finiter.cpp
@@ -0,0 +1,26 @@
+// demonstrates istream_iterator with files
+#include <iostream>
+#include <list>
+#include <fstream>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+int main()
+{
+    list<int> iList; //empty list
+    ifstream infile("ITER.DAT"); //create input file object
+//(ITER.DAT must already exist)
+//istream iterators
+    istream_iterator<int> file_iter(infile); //file
+    istream_iterator<int> end_of_stream; //eos
+//copy from infile to iList
+    copy( file_iter, end_of_stream, back_inserter(iList) );
+    cout << endl; //display iList
+    ostream_iterator<int> ositer(cout, "--");
+    copy(iList.begin(), iList.end(), ositer);
+    cout << endl;
+    return 0;
+}
+
+//11--21--31--41--51--
