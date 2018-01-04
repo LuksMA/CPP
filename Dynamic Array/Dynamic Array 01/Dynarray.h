5f048cd00c011e367a26c42adb53110fdabbcea4
vectormars <xuejie328@gmail.com>
2018-01-04 11:00:49 -0500

    Add files via upload

---
+++ Dynamic Array/Dynamic Array 01/Dynarray.h
@@ -0,0 +1,19 @@
+#ifndef DYNARRAY_H
+#define DYNARRAY_H
+
+
+class Dynarray
+{
+private:
+    int *pa;        // points to the array
+    int length;     // the # elements
+    int nextIndex;  // the next highest index value
+public:
+    Dynarray();                         // the constructor
+    ~Dynarray();                        // the destructor
+    int& operator[](int index);         // the indexing operation
+    void add(int val);                  // add a new value to the end
+    int size();                         // return length
+};
+
+#endif // DYNARRAY_H
---
+++ Dynamic Array/Dynamic Array 01/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Dynarray.h"
+
+using namespace std;
+
+int main()
+{
+    Dynarray da; // create an array object, size 10
+    da.add(1); // add values to the end
+    da.add(2);
+    da.add(3);
+    da[3] = 4; // use LHV for assignment
+
+    cout<<'[';
+        for (int i = 0; i < da.size(); i++) // get length of array using size()
+            cout << da[i] << ' '; // print out using RHV
+    cout<<"]\n";
+
+
+    da[12] = 5; // assign element past end of array
+    cout<<'[';
+        for (int j = 0; j < da.size(); j++) // size is now 22
+            cout << da[j] << ' '; // print out all elements again in
+    cout<<"]\n";
+
+    return 0;
+}
