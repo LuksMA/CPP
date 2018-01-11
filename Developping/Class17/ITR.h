bd58aa19d19af520391b47ee6219efc4280dd37d
Jie Xue <xuejie328@gmail.com>
2018-01-11 16:26:29 -0500

    Add files via upload

---
+++ Developping/Class17/ITR.h
@@ -0,0 +1,66 @@
+#ifndef ITR_H
+#define ITR_H
+
+#include "DataGeneration.h"
+#include "Result.h"
+#include "DataInfo.h"
+#include <iostream>
+
+using namespace std;
+
+class ITR
+{
+private:
+    vector<int> var_Type; // var_Size
+    int** var_X;     // sample_Size x var_Size
+    int** var_A;     // sample_Size x action_Size
+    double** var_Y;  // sample_Size x y_Size
+    bool*** table_X; // var_Size x cut_Size[i] x sample_Size
+
+    DataInfo* info;  // var_Size
+
+    int* cut_Size;   // var_Size
+
+    int sample_Size;   // row
+    int var_Size;      // column
+    int action_Size;
+    int y_Size;
+
+    double T0;
+
+public:
+    ITR(DataGeneration &data);
+    ~ITR();
+
+    int getSampleSize();
+    int getVarSize();
+    int getActionSize();
+    int getYSize();
+    int* get_CutSize(){ return cut_Size;};
+    int** get_X(){ return var_X;};
+    int** get_Action(){ return var_A;};
+    double** get_Y(){ return var_Y;};
+    bool*** get_X_Table(){ return table_X; };
+
+    void print_X();
+    void print_Action();
+    void print_Y();
+    void print_All();
+    void print_Range();
+    void print_Type();
+    void print_CutTable();
+
+    void threeDepthPrint();
+//    vector<Result *> threeDepth();
+
+private:
+    void load_Range();
+    void load_table_X(vector<vector<int>> x);
+    void load_X(vector<vector<int>> x);
+    void load_Action(vector<vector<int>> a);
+    void load_Y(vector<vector<double>> y);
+    void cleanAll();
+
+};
+
+#endif // ITR_H
---
+++ Developping/Class17/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <omp.h>
+
+#include "DataGeneration.h"
+#include "ITR.h"
+
+
+using namespace std;
+
+template<class T>
+void print1DVector(vector<T> const &vectIn);
+
+int main()
+{
+    /// Test data generation class
+//    vector<int> varType(35,0); /// 0 for cont, 1 for ordinal, 2 for nominal
+//    vector<double> rangesCont = {10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0};
+//    vector<int> rangesOrd = {};
+//    vector<int> rangesNom = {};
+//    vector<double> rangesY = {0.0,100.0};
+//    vector<int> rangesAction = {0,1};
+
+    vector<int> varType(25,0); /// 0 for cont, 1 for ordinal, 2 for nominal
+    vector<int> varType1(5,1);
+    vector<int> varType2(5,2);
+    varType.insert(varType.end(),varType1.begin(),varType1.end());
+    varType.insert(varType.end(),varType2.begin(),varType2.end());
+
+    vector<double> rangesCont = {50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0,0.0,100.0,10.0,200.0,50.0,300.0};
+    vector<int> rangesOrd = {0,4,0,4,0,4,0,4,0,4};
+    vector<int> rangesNom = {0,4,0,4,0,4,0,4,0,4};
+    vector<double> rangesY = {0.0,100.0};
+    vector<int> rangesAction = {0,1};
+
+    DataGeneration data(varType,rangesY,rangesAction,rangesCont,rangesOrd,rangesNom);
+    data.creatSamples(3000);
+    data.preprocessing();
+//    data.printDataSet();
+//    data.printAction();
+//    print1DVector(data.getVarType());
+//    cout<<'\n'<<data.getVarType().size();
+
+
+    ITR ITR_CS(data);
+//    ITR_CS.print_Type();
+//    cout<<endl;
+//    cout<<"Sample size: "<<ITR_CS.getSampleSize()<<endl;
+//    cout<<"Variable size: "<<ITR_CS.getVarSize()<<endl;
+//    cout<<"Action size: "<<ITR_CS.getActionSize()<<endl;
+//    cout<<"Y size: "<<ITR_CS.getYSize()<<endl;
+//    ITR_CS.print_All();
+//    ITR_CS.print_Range();cout<<endl;
+//    ITR_CS.print_CutTable();
+
+
+    ITR_CS.threeDepthPrint();
+//    ITR_CS.threeDepth();
+
+
+
+
+}
+
+
+
+template<class T>
+void print1DVector(vector<T> const &vectIn)
+{
+    cout<<"[ ";
+    for(const auto &row : vectIn)
+    {
+        cout<<row<<' ';
+    }
+    cout<<']';
+}
+
