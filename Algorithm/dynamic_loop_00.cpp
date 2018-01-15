2bdef75ac3a6d7e44c05c015b29718e4598bbd52
Jie Xue <xuejie328@gmail.com>
2018-01-15 13:28:59 -0500

    Add files via upload

---
+++ Algorithm/dynamic_loop_00.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using  namespace std;
+int main()
+{
+    for (int r=1; r<=3; r++)
+        for (int s=1; s<=3; s++)
+            for (int t=1; t<=3; t++)
+                cout << r << s << t << endl;  // print(r,s,t)
+}
