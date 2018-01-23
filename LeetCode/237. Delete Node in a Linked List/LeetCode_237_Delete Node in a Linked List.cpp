708fe0fb86664e56eb7c870692d64b1beb0ff4d0
Jie Xue <xuejie328@gmail.com>
2018-01-23 13:29:40 -0500

    Add files via upload

---
+++ LeetCode/237. Delete Node in a Linked List/LeetCode_237_Delete Node in a Linked List.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+void printList(struct ListNode *n)
+{
+    while (n != NULL)
+    {
+        cout<<n->val<<' ';
+        n = n->next;
+    }
+    cout<<endl;
+}
+
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (!node || !node->next) {
+            return;
+        }
+        auto node_to_delete = node->next;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        delete node_to_delete;
+    }
+};
+
+int main()
+{
+    ListNode *l1 = new ListNode(1);
+    ListNode *l1_1 = new ListNode(2);   l1->next = l1_1;
+    ListNode *l1_2 = new ListNode(2);   l1_1->next = l1_2;
+    ListNode *l1_3 = new ListNode(4);   l1_2->next = l1_3;
+//    ListNode *l1_4 = new ListNode(4);   l1_3->next = l1_4;
+//    ListNode *l1_5 = new ListNode(5);   l1_4->next = l1_5;
+//    ListNode *l1_6 = new ListNode(6);   l1_5->next = l1_6;
+
+//    printList(l1);
+
+    Solution s;
+
+}
