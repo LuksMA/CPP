3a10c42125cb248c889f1de5fe5a2363c7667941
Jie Xue <xuejie328@gmail.com>
2018-01-30 12:58:02 -0500

    Add files via upload

---
+++ Data Structure/Linked List/Single Linked List/Single Linked List 01 Get Nth Node Value.cpp
@@ -0,0 +1,57 @@
+// https://www.geeksforgeeks.org/write-a-function-to-get-nth-node-in-a-linked-list/
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+void printList(struct ListNode *n)
+{
+    while (n != nullptr)
+    {
+        cout<<n->val<<' ';
+        n = n->next;
+    }
+}
+
+
+int GetNth(ListNode* head, int index)
+{
+    int i = 0;
+    while(head){
+        ++i;
+        head = head->next;
+        if(i==index){
+            return head->val;
+        }
+    }
+    return NULL;
+}
+
+
+int main()
+{
+    ListNode *ls_0 = new ListNode(10);
+    ListNode *ls_1 = new ListNode(15);
+    ls_0->next = ls_1;
+    ListNode *ls_2 = new ListNode(12);
+    ls_1->next = ls_2;
+    ListNode *ls_3 = new ListNode(13);
+    ls_2->next = ls_3;
+    ListNode *ls_4 = new ListNode(20);
+    ls_3->next = ls_4;
+    ListNode *ls_5 = new ListNode(14);
+    ls_4->next = ls_5;
+
+    printList(ls_0);
+    cout<<endl;
+
+    cout<<GetNth(ls_0,2)<<endl;
+    cout<<GetNth(ls_0,10)<<endl;
+}
