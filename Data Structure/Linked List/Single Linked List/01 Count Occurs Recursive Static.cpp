d6114613f9239fcfb423a10694239ab3155185c8
Jie Xue <xuejie328@gmail.com>
2018-02-08 20:45:39 -0500

    Add files via upload

---
+++ Data Structure/Linked List/Single Linked List/01 Count Occurs Recursive Static.cpp
@@ -0,0 +1,48 @@
+// https://www.geeksforgeeks.org/write-a-function-that-counts-the-number-of-times-a-given-int-occurs-in-a-linked-list/
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+void printList(struct ListNode *n){
+  while (n != NULL)
+  {
+     cout<<n->val<<' ';
+     n = n->next;
+  }
+}
+
+
+
+int count(struct ListNode* head, int search_for){
+    static int frequency = 0;
+    if(head == nullptr) return frequency;
+    if(head->val == search_for) frequency++;
+    count(head->next, search_for);
+}
+
+
+
+int main(){
+    ListNode *ls_0 = new ListNode(1);
+    ListNode *ls_1 = new ListNode(2);  ls_0->next = ls_1;
+    ListNode *ls_2 = new ListNode(1);  ls_1->next = ls_2;
+    ListNode *ls_3 = new ListNode(10); ls_2->next = ls_3;
+    ListNode *ls_4 = new ListNode(20); ls_3->next = ls_4;
+    ListNode *ls_5 = new ListNode(1); ls_4->next = ls_5;
+
+    cout<<"Created Linked List: "<<endl;
+    printList(ls_0);cout<<endl;
+
+    cout<<count(ls_0,1);cout<<endl;
+}
+
+//Created Linked List:
+//1 2 1 10 20 1
+//3
