b9631fa346fe6cfea4f023aade394571472da040
Jie Xue <xuejie328@gmail.com>
2018-02-22 16:03:27 -0500

    Add files via upload

---
+++ Test/GoogleTest/SampleCase4/sample4.h
@@ -0,0 +1,15 @@
+#pragma once
+class Counter {
+private:
+	int counter_;
+
+public:
+	// Creates a counter that starts at 0.
+	Counter() : counter_(0) {}
+
+	// Returns the current counter value, and increments it.
+	int Increment();
+
+	// Prints the current counter value to STDOUT.
+	void Print() const;
+};
